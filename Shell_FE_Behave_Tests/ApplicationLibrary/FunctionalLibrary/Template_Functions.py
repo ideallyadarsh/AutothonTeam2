398623baacc9812361b2eba9fb95d4b8214b5589
Aryan Gupta <aryanjigupta@gmail.com>
2025-09-15 15:15:08 +0530

    2023 web solution

---
+++ Shell_FE_Behave_Tests/ApplicationLibrary/FunctionalLibrary/Template_Functions.py
@@ -0,0 +1,55 @@
+import time
+from Shell_FE_Behave_Tests.ApplicationLibrary.ControlLibrary.Template_Controls import TemplateControls
+from Shell_FE_Selenium_Core.SeleniumBase import SeleniumBase
+from Shell_FE_Selenium_Core.Utilities.BrowserUtilities import BrowserUtilities
+from Shell_FE_Selenium_Core.Utilities.SeleniumUtilities import SeleniumUtilities
+from Shell_FE_Selenium_Core.Utilities.WaitUtilities import WaitUtilities
+from Shell_FE_Requests_Core.RequestsBase import RequestsBase
+from Shell_FE_Requests_Core.Utilities.AssertionUtilities import AssertionUtilities
+from Shell_FE_Selenium_Core.Utilities.FileUtilities import FileUtilities
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.common.by import By
+
+
+class TemplateFunctions:
+
+    def __init__(self):
+        self.autoControls = TemplateControls(SeleniumBase.driver)
+        self.article_details = []
+        self.base_url = "http://ec2-54-254-162-245.ap-southeast-1.compute.amazonaws.com:9000/items/"
+        self.team_name = "Shell India - 1"
+
+    def navigate_to_x(self):
+        BrowserUtilities.maximize_window()
+        BrowserUtilities.navigate_to_url(SeleniumBase.url)
+       
+    def click_on_sign_in_button(self):
+        WaitUtilities.wait_for_element_to_be_visible(self.autoControls.signin_button, 120)
+        SeleniumUtilities.click_element(self.autoControls.get_signin_button())
+       
+    def enter_username_and_click_next(self):
+        SeleniumUtilities.send_text(self.autoControls.get_username_input_field(), "mister_gupta")
+        SeleniumUtilities.click_element(self.autoControls.get_next_button())
+    
+    def enter_password_and_click_log_in(self):
+        SeleniumUtilities.send_text(self.autoControls.get_password_input_field(), "kcabinARYAN")
+        SeleniumUtilities.click_element(self.autoControls.get_login_button())
+        WaitUtilities.wait_for_url_to_match_value("https://x.com/home", 60)
+
+    def make_posts_and_verify_success(self):
+        tweet_inputs = FileUtilities.read_json_file_as_dictionary("Tweets.json")
+        WaitUtilities.wait_for_element_to_be_visible(self.autoControls.tweet_compose_textarea, 120)
+        for tweet in tweet_inputs["tweets"]:
+            SeleniumUtilities.send_text(self.autoControls.get_tweet_compose_textarea(), tweet)
+            SeleniumUtilities.click_element(self.autoControls.get_post_button())
+            time.sleep(5)
+        
+    def search_three_posts_and_verify_results(self):
+        SeleniumUtilities.click_element(self.autoControls.get_search_button())
+        WaitUtilities.wait_for_element_to_be_visible(self.autoControls.search_input_field, 60)
+        SeleniumUtilities.send_text(self.autoControls.get_search_input_field(), "sign_revlis15sep")
+        SeleniumUtilities.click_element(self.autoControls.get_search_result_button())
+
+    def take_screenshot_of_posts_and_save_locally(self):
+        WaitUtilities.wait_for_element_to_be_visible(self.autoControls.tweet, 60)
+        self.autoControls.get_tweet().screenshot("tweet_textarea.png")
---
+++ Shell_FE_Behave_Tests/features/steps/Template_steps.py
@@ -0,0 +1,37 @@
+from behave import *
+from Shell_FE_Behave_Tests.MobileApplicationLibrary.FunctionalLibrary.TestAutothon_Functions import TestAutothon_Functions
+from Shell_FE_Behave_Tests.ApplicationLibrary.FunctionalLibrary.Template_Functions import TemplateFunctions
+from Shell_FE_Selenium_Core.SeleniumBase import SeleniumBase
+from Shell_FE_Selenium_Core.Utilities.BrowserUtilities import BrowserUtilities
+from selenium.webdriver.common.by import By
+from Shell_FE_Selenium_Core.Utilities.SeleniumUtilities import SeleniumUtilities
+
+@given(u'I Navigate to X')
+def step_impl(context):
+    context.functions = TemplateFunctions()
+    context.functions.navigate_to_x()
+
+@when(u'I Click on Sign in button')
+def step_impl(context):
+    context.functions.click_on_sign_in_button()
+
+@when(u'I Enter email/username and click next')
+def step_impl(context):
+    context.functions.enter_username_and_click_next()
+
+@when(u'I Enter password and click Log in button')
+def step_impl(context):
+    context.functions.enter_password_and_click_log_in()
+
+@then(u'I make posts and verify successful response')
+def step_impl(context):
+    context.functions.make_posts_and_verify_success()
+
+@then(u'I search the three posts and verify the search results')
+def step_impl(context):
+    context.functions.search_three_posts_and_verify_results()
+
+
+@then(u'I take a screenshot of the posts and save it in local')
+def step_impl(context):
+    context.functions.take_screenshot_of_posts_and_save_locally()
